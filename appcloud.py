77e40abbb7d1a9f3fa055130e893404bb4bd05bb
Illia <ikunin@tulane.edu>
2025-04-03 12:08:28 -0500

    a

--- appcloud.py
+++ appcloud.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 button= st.button("Click me!")
+df= None
 if button:
     df= st.file_uploader("Upload a CSV file", type=["csv"])
-    if df:
-        st.dataframe(df)
+
+if df:
+    st.dataframe(df)
