7a7620567f7ea3be67ac091bd306ef6efa4b8821
Illia <ikunin@tulane.edu>
2025-04-03 12:05:07 -0500

    a

--- appcloud.py
+++ appcloud.py
@@ -3,5 +3,4 @@ import pandas as pd
 button= st.button("Click me!")
 if button:
     df= st.file_uploader("Upload a CSV file", type=["csv"])
-st.write("# Welcome to the AppCloud!")
 st.dataframe(df.head())
